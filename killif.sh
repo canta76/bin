bca4da8a6fa4539fdc8f281e3969cf62ddf6dc1e
max2 <max2@local>
2017-09-16 20:02:33 +0200

    -

--- killif.sh
+++ killif.sh
@@ -5,6 +5,16 @@
   echo "freemem: "$SIZE
   if [ "$SIZE" -gt "2000" ]; then
     echo "ram ok"
+    # guardo se sta girando
+    RES=$(ps | grep "node /root/comm/comm.js" | grep -v grep)
+    echo "result: "$RES
+    if [ "${#RES}" -gt 0 ];
+    then
+        echo "comm running"
+    else
+        echo "starting comm"
+        node /root/comm/comm.js > /dev/null 2>&1 &
+    fi
   else
     echo "ram low, kill comm process"
     /root/bin/pkill.sh comm/
