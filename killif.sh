6845f3e24846e76507f6b7f33d372ca664eff4da
max2 <max2@local>
2017-09-16 18:39:49 +0200

    -

---
+++ killif.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+  SIZE=$(grep MemFree /proc/meminfo | awk '{print $2}')
+  sleep 1
+  echo "freemem: "$SIZE
+  if [ "$SIZE" -gt "21500" ]; then
+    echo "ram ok"
+  else
+    echo "ram low, kill comm process"
+    /root/bin/pkill.sh comm/
+    sleep 2
+    echo "restarting comm"
+    node /root/comm/comm.js > /dev/null 2>&1 &
+  fi;
